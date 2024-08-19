9b4921075e00fa3dea6044b5a8679eee2c773032
yty <yuty@zju.edu.cn>
2024-08-20 01:19:16 +0800

    修改input格式

--- DCHMM/train_classify.py
+++ DCHMM/train_classify.py
@@ -38,7 +38,7 @@ def train(args, hmm_model: Model, train_dataset, batch_size, optimizer_hmm,
     for i_batch, (sample_batched) in enumerate(train_loader):  # 第i个batch
 
         input = torch.tensor(sample_batched[0], device=args.device)
-        print(input)
+        # print(input)
         gt_y = torch.tensor(sample_batched[1], device=args.device)
 
         # args.h_size
@@ -51,15 +51,19 @@ def train(args, hmm_model: Model, train_dataset, batch_size, optimizer_hmm,
         logit = None
         for index in range(len(input[0])):  # the len of time series
             if index == 0:
-                input_t = input[:, index]
+                # 修改点 修改格式
+                # input_t = input[:, index]
+                input_t = input
                 input_t_last = torch.zeros_like(input_t)
                 h_t_last = h0
             else:
-                input_t = input[:, index]
+                # 修改点 修改格式
+                input_t = input
+                # input_t = input[:, index]
                 input_t_last = input[:, index - 1]
             if input_t.sum() == 0:
                 break
-            print(f"input_t shape: {input_t.shape}")  # 添加这一行来打印 input_t 的形状
+            # print(f"input_t shape: {input_t.shape}")  # 添加这一行来打印 input_t 的形状
             vae_rec_input_t, mu_h, logvar_h, mu_h_prior, logvar_h_prior \
                 = hmm_model(input_t)
 
